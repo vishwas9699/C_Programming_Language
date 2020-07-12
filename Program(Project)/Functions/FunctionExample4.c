c8e22033c1407b9bd6f1a623ebb85d9d92397109
vishwas9699 <vvishwas179@gmail.com>
2020-07-12 12:55:45 +0530

    ic

--- Program(Project)/Functions/FunctionExample4.c
+++ Program(Project)/Functions/FunctionExample4.c
@@ -13,7 +13,3 @@ int square()
     scanf("%f",&side);
     return side * side;
 }
-
-//Going to calculate the area of the square
-//Enter the length of the side in meters: 10
-//The area of the square: 100.000000

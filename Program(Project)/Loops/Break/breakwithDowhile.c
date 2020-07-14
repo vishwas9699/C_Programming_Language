3954ed184c41f3e43c73b07e1b86c97fcc590f93
vishwas9699 <vvishwas179@gmail.com>
2020-07-14 23:11:04 +0530

    ic

--- Program(Project)/Loops/Break/breakwithDowhile.c
+++ Program(Project)/Loops/Break/breakwithDowhile.c
@@ -19,26 +19,3 @@ void main ()
         n++;
     }while(1);
 }
-
-//2 X 1 = 2
-//2 X 2 = 4
-//2 X 3 = 6
-//2 X 4 = 8
-//2 X 5 = 10
-//2 X 6 = 12
-//2 X 7 = 14
-//2 X 8 = 16
-//2 X 9 = 18
-//2 X 10 = 20
-//do you want to continue with the table of 3 , enter any non-zero value to continue.1
-//3 X 1 = 3
-//3 X 2 = 6
-//3 X 3 = 9
-//3 X 4 = 12
-//3 X 5 = 15
-//3 X 6 = 18
-//3 X 7 = 21
-//3 X 8 = 24
-//3 X 9 = 27
-//3 X 10 = 30
-//do you want to continue with the table of 4 , enter any non-zero value to continue.0

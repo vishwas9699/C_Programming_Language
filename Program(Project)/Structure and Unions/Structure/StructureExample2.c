129069070d5e2dc0fb86cb0b6b150ef2be35d4d7
vishwas9699 <vvishwas179@gmail.com>
2020-07-16 13:17:21 +0530

    ic

--- Program(Project)/Structure and Unions/Structure/StructureExample2.c
+++ Program(Project)/Structure and Unions/Structure/StructureExample2.c
@@ -8,14 +8,14 @@ struct employee
 int main( )
 {
     //store first employee information
-    e1.id=101;
-    strcpy(e1.name, "Sonoo Jaiswal");//copying string into char array
+    e1.id=29;
+    strcpy(e1.name, "Vishwas");//copying string into char array
     e1.salary=56000;
 
     //store second employee information
-    e2.id=102;
-    strcpy(e2.name, "James Bond");
-    e2.salary=126000;
+    e2.id=26;
+    strcpy(e2.name, "Vishwesh");
+    e2.salary=56000;
 
     //printing first employee information
     printf( "employee 1 id : %d\n", e1.id);
@@ -30,9 +30,3 @@ int main( )
 }
 
 
-//employee 1 id : 101
-//employee 1 name : Sonoo Jaiswal
-//employee 1 salary : 56000.000000
-//employee 2 id : 102
-//employee 2 name : James Bond
-//employee 2 salary : 126000.000000

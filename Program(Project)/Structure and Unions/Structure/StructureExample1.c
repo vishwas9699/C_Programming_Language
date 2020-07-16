1066329f84c96ada2db8dd9b2ea62cc7a7b8bb3b
vishwas9699 <vvishwas179@gmail.com>
2020-07-16 13:16:09 +0530

    ic

--- Program(Project)/Structure and Unions/Structure/StructureExample1.c
+++ Program(Project)/Structure and Unions/Structure/StructureExample1.c
@@ -7,13 +7,10 @@ struct employee
 int main( )
 {
     //store first employee information
-    e1.id=101;
-    strcpy(e1.name, "Sonoo Jaiswal");//copying string into char array
+    e1.id=29;
+    strcpy(e1.name, "Vishwas");//copying string into char array
     //printing first employee information
     printf( "employee 1 id : %d\n", e1.id);
     printf( "employee 1 name : %s\n", e1.name);
     return 0;
 }
-
-//employee 1 id : 101
-//employee 1 name : Sonoo Jaiswal

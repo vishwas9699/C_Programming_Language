49f1401c3743cdfaf3d657c9ed8c5b9aceffa05a
vishwas9699 <vvishwas179@gmail.com>
2020-07-16 12:46:06 +0530

    ic

---
+++ Program(Project)/printf&scanf/ProgramtoPrintCube.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main(){
+    int number;
+    printf("enter a number:");
+    scanf("%d",&number);
+    printf("cube of number is:%d ",number*number*number);
+    return 0;
+}
+
+// enter a number:5
+//cube of number is:125
---
+++ Program(Project)/printf&scanf/SumOfTwoNum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int x=0,y=0,result=0;
+
+    printf("enter first number:");
+    scanf("%d",&x);
+    printf("enter second number:");
+    scanf("%d",&y);
+
+    result=x+y;
+    printf("sum of 2 numbers:%d ",result);
+
+    return 0;
+}
+
+// enter first number:9
+// enter second number:9
+// sum of 2 numbers:18
